e42578e7ee9b0d7c4f6b27abbd4b76008665582c
coolkim99 <coolkim99@sookmyung.ac.kr>
2021-01-29 18:51:47 +0900

    logout

--- Listener/frontend/src/pages/MainPage.js
+++ Listener/frontend/src/pages/MainPage.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './main.css'
+import HeaderContainer from '../containers/common/HeaderContainer';
 
 function main() {
   return (
-      <div className="mainpage">
-        <header>
-            Home Training
-        </header>
-        <span><button id="logout">logout</button></span>
-        <nav>
-            <span>오늘의 운동</span>
-            <span>운동 일지</span>
-            <span>마이페이지</span>
-        </nav>
-     </div>
+    <>
+      <HeaderContainer />
+      <nav>
+          <span>오늘의 운동</span>
+          <span>운동 일지</span>
+          <span>마이페이지</span>
+      </nav>
+    </>
   );
 }
 export default main;
--- Listener/frontend/src/pages/PostListPage.js
+++ Listener/frontend/src/pages/PostListPage.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 const PostListPage = ( { history } ) => {
     return (
-        <div className="App">
-          <div className ="main_div">
-            <div className="main">
-              HomeTraining
-            </div>
-            <div className = "bnt_div">
-              <button className = "button" onClick ={ () =>
-                {history.push("/login")}}>SignIn</button>
-              <button className = "button" onClick ={ () =>
-                {history.push("/register")}}>SignUp</button>
-            </div>
-          </div>
-      </div> 
+      <div className="App">
+      <div className ="main_div">
+        <div className="main" onClick ={ () =>
+            {history.push("/main")}}>
+          HomeTraining
+        </div>
+        <div className = "bnt_div">
+          <button className = "button" onClick ={ () =>
+            {history.push("/login")}}>SignIn</button>
+          <button className = "button" onClick ={ () =>
+            {history.push("/register")}}>SignUp</button>
+        </div>
+      </div>
+  </div> 
     )
 };
 
