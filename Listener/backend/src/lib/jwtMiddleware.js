5e117255e5ef23663dce91dc413239b5781907a6
coolkim99 <coolkim99@sookmyung.ac.kr>
2021-03-18 21:10:26 +0900

    commit5

--- Listener/backend/src/lib/jwtMiddleware.js
+++ Listener/backend/src/lib/jwtMiddleware.js
@@ -1,11 +1,14 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/user';
 
+
+
 const jwtMiddleware = async (ctx, next) => {
+  JWT_SECRET="8f386f672542e4c095dae727aa6f1314b690b0529805daae0bcb67119346b8df38e7478dd25afc80cb837a28e9c5e3ea2e8fbdb903100ca4fb5cdda40ae529e7";
   const token = ctx.cookies.get('access_token');
   if (!token) return next(); // 토큰이 없음
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, JWT_SECRET);
     const user = await User.findById(decoded._id);
     ctx.state.user = {
       _id: decoded._id,
--- Listener/backend/src/main.js
+++ Listener/backend/src/main.js
@@ -5,9 +5,9 @@ import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware'
-//import createFakeData from './createFakeData';
 
-const { PORT, MONGO_URI } = process.env;
+PORT=4000;
+MONGO_URI="mongodb+srv://jokj624:290315ab@youtube-clone.8yicy.mongodb.net/test";
 
 mongoose
 .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology:true})
@@ -29,6 +29,6 @@ app.use(jwtMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
 
 const port = PORT;
-app.listen(4000, () => {
+app.listen(port, () => {
     console.log("Listening to port 4000");
 });
